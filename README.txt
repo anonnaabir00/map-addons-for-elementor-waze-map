d1398d79e64e4d30e7948786f2bc348ce9a76fb3
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-11-09 12:50:48 +0600

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,47 @@
+# Waze Map For Elementor
+Contributors: asadabir, nervythemes
+Tags: elementor, waze map, google map, open street map
+Donate link: https://nervythemes.com/
+Requires at least: 3.0.1
+Tested up to: 6.4.1
+Requires PHP: 7.2
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Easily intregate Waze map, Bing Maps on Elementor.
+
+# Description
+
+Map Addons allows you to easily intregate Waze map & Bing Maps on an Elementor based site.
+
+
+How to use
+
+* Install the plugin
+* Go to Waze Map official website: [Waze Map](https://www.waze.com/livemap/)
+* Search with your location
+* Get the Latitude and Longitude form bottom corner: [Screenshot](https://prnt.sc/uxul6v)
+* Place it to the Waze Map addon.
+* Done
+
+
+== Frequently Asked Questions ==
+
+Qus: Does the plugin require any special settings/plugins?
+Ans: You must install and activate Elementor before using this plugin.
+
+
+== Screenshots ==
+1. Waze Map Addons For Elementor
+2. Waze Map Options
+
+== Changelog ==
+
+1.0: Initial Release
+
+1.1:
+
+- Tested up to latest WordPress 6.4.1
+- Add Bing Maps Addon (Basic)
+- README Updated
---
+++ map-addons-elementor.php
@@ -0,0 +1,191 @@
+<?php
+/**
+ *
+ * @link              https://codember.com/
+ * @since             1.1
+ * @package           Map Addons For Elementor
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Waze Map, Bing Maps For WordPress & Elementor
+ * Plugin URI:        https://codember.com/
+ * Description: 	  Easily intregate Waze map & Bing Maps on your wordpress & Elementor website.
+ * Version:           1.1
+ * Author:            NervyThemes
+ * Author URI:        https://codember.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: 	  map-addons-elementor
+ * Tested up to:      6.4.1
+
+ */
+
+
+	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+	/**
+	 * @since 1.0
+	 */
+
+	final class Map_Addons_Elementor {
+
+		/**
+		 * @since 1.0
+		 * @var string The plugin version.
+		 */
+		const VERSION = '1.0';
+
+		/**
+		 * Minimum Elementor Version
+		 *
+		 * @since 1.0
+		 * @var string Minimum Elementor version required to run the plugin.
+		 */
+		const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+
+		/**
+		 * Minimum PHP Version
+		 *
+		 * @since 1.2.0
+		 * @var string Minimum PHP version required to run the plugin.
+		 */
+		const MINIMUM_PHP_VERSION = '7.0';
+
+		/**
+		 * Constructor
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 */
+
+		public function __construct() {
+
+			// Load translation
+			add_action( 'init', array( $this, 'i18n' ) );
+
+			// Init Plugin
+			add_action( 'plugins_loaded', array( $this, 'init' ) );
+		}
+
+		/**
+		 * Load Textdomain
+		 *
+		 * Load plugin localization files.
+		 * Fired by `init` action hook.
+		 *
+		 * @since 1.0
+		 * @access public
+		 */
+		public function i18n() {
+			load_plugin_textdomain( 'map-addons-elementor' );
+		}
+
+		/**
+		 * Initialize the plugin
+		 *
+		 * Fired by `plugins_loaded` action hook.
+		 *
+		 * @since 1.2.0
+		 * @access public
+		 */
+		public function init() {
+
+			// Check if Elementor installed and activated
+			if ( ! did_action( 'elementor/loaded' ) ) {
+				add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
+				return;
+			}
+
+
+			// Check for required Elementor version
+			if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+				add_action( 'admin_notices', array( $this, 'admin_notice_minimum_elementor_version' ) );
+				return;
+			}
+
+			// Check for required PHP version
+			if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+				add_action( 'admin_notices', array( $this, 'admin_notice_minimum_php_version' ) );
+				return;
+			}
+
+			// Once we get here, We have passed all validation checks so we can safely include our plugin
+			require_once( 'plugin.php' );
+		}
+
+		/**
+		 * Admin notice
+		 *
+		 * Warning when the site doesn't have Elementor installed or activated.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 */
+
+		public function admin_notice_missing_main_plugin() {
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+
+
+			$message = sprintf(
+				/* translators: 1: Plugin name 2: Elementor */
+				esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'map-addons-elementor' ),
+				'<strong>' . esc_html__( 'Waze Map For Elementor', 'map-addons-elementor' ) . '</strong>',
+				'<strong>' . esc_html__( 'Elementor', 'map-addons-elementor' ) . '</strong>'
+			);
+
+			printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+		}
+
+		/**
+		 * Admin notice
+		 *
+		 * Warning when the site doesn't have a minimum required Elementor version.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 */
+		public function admin_notice_minimum_elementor_version() {
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+
+			$message = sprintf(
+				/* translators: 1: Plugin name 2: Elementor 3: Required Elementor version */
+				esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'map-addons-elementor' ),
+				'<strong>' . esc_html__( 'Waze Map For Elementor', 'map-addons-elementor' ) . '</strong>',
+				'<strong>' . esc_html__( 'Elementor', 'map-addons-elementor' ) . '</strong>',
+				self::MINIMUM_ELEMENTOR_VERSION
+			);
+
+			printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+		}
+
+		/**
+		 * Admin notice
+		 *
+		 * Warning when the site doesn't have a minimum required PHP version.
+		 *
+		 * @since 1.0.0
+		 * @access public
+		 */
+		public function admin_notice_minimum_php_version() {
+			if ( isset( $_GET['activate'] ) ) {
+				unset( $_GET['activate'] );
+			}
+
+			$message = sprintf(
+				/* translators: 1: Plugin name 2: PHP 3: Required PHP version */
+				esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'map-addons-elementor' ),
+				'<strong>' . esc_html__( 'Waze Map For Elementor', 'map-addons-elementor' ) . '</strong>',
+				'<strong>' . esc_html__( 'PHP', 'map-addons-elementor' ) . '</strong>',
+				self::MINIMUM_PHP_VERSION
+			);
+
+			printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
+		}
+	}
+
+
+	new Map_Addons_Elementor();
